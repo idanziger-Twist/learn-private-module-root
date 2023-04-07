0200a32d5b8f2d4f832b1b47fcacfdaa19f71d1b
iDanziger <idanziger@twistbioscience.com>
2023-04-07 17:02:41 +0300

    BS fuck

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = var.region
+  assume_role {
+    role_arn = "arn:aws:iam::176102690181:user/terraform-user"
 }
+  }
+  
+
 
 module "s3-webapp" {
   source  = "app.terraform.io/Gooner/s3-webapp/aws"
