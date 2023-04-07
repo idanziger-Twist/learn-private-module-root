de15e74fe206bf6653451ab85cd2195a55bea31f
iDanziger <idanziger@twistbioscience.com>
2023-04-07 16:00:13 +0300

    module update

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+module "s3-webappapp" {
+  source  = "app.terraform.io/Gooner/s3-webappapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
   version = "1.0.0"
 }
+
+
