2e36c3c8dc5f4c55c4bbd499cd17916ea1669567
iDanziger <idanziger@twistbioscience.com>
2023-04-07 16:21:08 +0300

    BS fuck

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,23 @@
+# https://registry.terraform.io/providers/hashicorp/aws/3.74.2
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "4.21.0"
+      source = "hashicorp/aws"
+      version = = "3.74.2"
     }
   }
 }
+
+
+
+
+
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "4.21.0"
+#     }
+#   }
+# }
